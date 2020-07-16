6ab78287c01431a30aa9ad6d115abb9c7506fd9b
mmrakt <mimuraakkun3@gmail.com>
2020-07-16 17:43:57 +0900

    :shower: index.jsの不要な記述を削除

--- store/index.js
+++ store/index.js
@@ -1,23 +1,5 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
 import { vuexfireMutations } from 'vuexfire'
-import post from './post'
-import auth from './auth'
-import status from './status'
 
-Vue.use(Vuex)
-
-const store = () => {
-  return new Vuex.Store({
-    modules: {
-      post,
-      auth,
-      status,
-    },
-    mutations: {
-      ...vuexfireMutations,
-    },
-  })
+export const mutations = {
+  ...vuexfireMutations,
 }
-
-export default store
