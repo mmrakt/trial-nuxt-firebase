3d684c66abb276047a6069c9355341b134270b5a
mmrakt <mimuraakkun3@gmail.com>
2020-07-24 12:34:21 +0900

    :+1: signup時にdisplayNameを変更できるように

--- pages/auth/signup.vue
+++ pages/auth/signup.vue
@@ -1,70 +1,73 @@
 <template>
-  <v-content>
-    <v-card width="600px" class="mx-auto my-10">
-      <v-card-title class="text-md-center">新規会員登録フォーム</v-card-title>
-      <v-divider></v-divider>
-      <ValidationObserver ref="obs">
-        <v-form ref="form">
-          <v-container class="px-10">
-            <ValidationProvider
-              v-slot="{ errors, valid }"
-              rules="required|alpha_num"
-              name="ユーザーID"
-            >
-              <v-text-field
-                v-model="userId"
-                prepend-icon="mdi-account-circle"
-                name="userId"
-                type="text"
-                label="ユーザーID(半角英数字)"
-                :error-messages="errors"
-                :success="valid"
-                alpha_num
-                required
-              />
-            </ValidationProvider>
+  <v-app>
+    <v-main>
+      <v-container class="py-5">
+        <v-row justify="center">
+          <v-sheet class="text-h4 py-5">Sign up</v-sheet>
+        </v-row>
+        <v-card width="600px" class="mx-auto my-10">
+          <ValidationObserver ref="obs">
+            <v-form ref="form">
+              <v-container class="px-10">
+                <ValidationProvider
+                  v-slot="{ errors, valid }"
+                  rules="required|alpha_num"
+                  name="username"
+                >
+                  <v-text-field
+                    v-model="userName"
+                    prepend-icon="mdi-account-circle"
+                    name="username"
+                    type="text"
+                    label="username"
+                    :error-messages="errors"
+                    :success="valid"
+                    alpha_num
+                    required
+                  />
+                </ValidationProvider>
 
-            <ValidationProvider
-              v-slot="{ errors, valid }"
-              rules="required|email"
-              name="メールアドレス"
-            >
-              <v-text-field
-                id="email"
-                v-model="email"
-                prepend-icon="mdi-email"
-                name="email"
-                type="text"
-                label="メールアドレス"
-                :error-messages="errors"
-                :success="valid"
-                required
-                email
-              />
-            </ValidationProvider>
-            <validation-provider
-              v-slot="{ errors, valid }"
-              rules="required|min:8|alpha_num"
-              name="パスワード"
-            >
-              <v-text-field
-                id="password"
-                ref="password"
-                v-model="password"
-                prepend-icon="mdi-lock"
-                :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-                name="password"
-                label="パスワード"
-                :type="showPassword ? 'text' : 'password'"
-                :error-messages="errors"
-                :success="valid"
-                required
-                min
-                alpha_num
-                @click:append="showPassword = !showPassword"
-              ></v-text-field>
-            </validation-provider>
-            <!--<validation-provider
+                <ValidationProvider
+                  v-slot="{ errors, valid }"
+                  rules="required|email"
+                  name="email"
+                >
+                  <v-text-field
+                    id="email"
+                    v-model="email"
+                    prepend-icon="mdi-email"
+                    name="email"
+                    type="text"
+                    label="email"
+                    :error-messages="errors"
+                    :success="valid"
+                    required
+                    email
+                  />
+                </ValidationProvider>
+                <validation-provider
+                  v-slot="{ errors, valid }"
+                  rules="required|min:8|alpha_num"
+                  name="password"
+                >
+                  <v-text-field
+                    id="password"
+                    ref="password"
+                    v-model="password"
+                    prepend-icon="mdi-lock"
+                    :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                    name="password"
+                    label="password"
+                    :type="showPassword ? 'text' : 'password'"
+                    :error-messages="errors"
+                    :success="valid"
+                    required
+                    min
+                    alpha_num
+                    @click:append="showPassword = !showPassword"
+                  ></v-text-field>
+                </validation-provider>
+                <!--<validation-provider
               v-slot="{ errors, valid }"
               rules="required|min:8|alpha_num|confirmed:password"
               name="パスワード"
@@ -82,15 +85,23 @@
                 alpha_num
                 confirmed
               ></v-text-field>
-            </validation-provider>-->
+                </validation-provider>-->
 
-            <!-- <date-picker v-model="birthday" /> -->
-            <v-btn color="primary" @click="signUp">登録</v-btn>
-          </v-container>
-        </v-form>
-      </ValidationObserver>
-    </v-card>
-  </v-content>
+                <!-- <date-picker v-model="birthday" /> -->
+                <v-btn
+                  color="primary"
+                  style="text-transform: none;"
+                  @click="signUp"
+                >
+                  Sign up
+                </v-btn>
+              </v-container>
+            </v-form>
+          </ValidationObserver>
+        </v-card>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
@@ -107,11 +118,9 @@ export default {
   },
   data() {
     return {
-      userId: '',
+      userName: '',
       email: '',
       password: '',
-      confirmPassword: '',
-      birthday: '',
       showPassword: false,
     }
   },
@@ -123,17 +132,20 @@ export default {
         const firebaseUser = await firebase
           .auth()
           .createUserWithEmailAndPassword(this.email, this.password)
-        // firestoreにアカウント情報を格納
-        await this.writeUserData(firebaseUser.user.uid, firebaseUser.user.email)
+        await firebaseUser.user.updateProfile({
+          displayName: this.userName,
+        })
+        await this.writeUserData(firebaseUser.user.uid)
         await this.login(firebaseUser.user.uid)
         this.$router.push('/protected')
       } catch (error) {
         console.log(error.message)
       }
     },
-    writeUserData(userId, email) {
-      userRef.doc(userId).set({
-        email,
+    writeUserData(uid) {
+      userRef.doc(uid).set({
+        userName: this.userName,
+        email: this.email,
       })
     },
   },
