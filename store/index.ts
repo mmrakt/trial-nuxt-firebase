19643eb7571403fa4888fe84d00da12335f9576b
mmrakt <mimuraakkun3@gmail.com>
2020-08-09 03:02:41 +0900

    :shower: userの同期化に伴いssr時の処理を削除

--- store/index.ts
+++ store/index.ts
@@ -1,20 +1,20 @@
 import { vuexfireMutations } from 'vuexfire'
-import { getUserFromCookie } from '@/helpers'
+// import { getUserFromCookie } from '@/helpers'
 
 export const mutations = {
   ...vuexfireMutations,
 }
 
-export const actions = {
-  async nuxtServerInit({ dispatch }: { dispatch: any }, { req }: { req: any }) {
-    const user: any = getUserFromCookie(req) // JWTをデコードしたトークンを取得
-    if (user) {
-      await dispatch('user/setUser', {
-        name: user.displayName,
-        email: user.email,
-        avatar: user.photoURL,
-        uid: user.user_id,
-      })
-    }
-  },
-}
+// export const actions = {
+//   async nuxtServerInit({ dispatch }: { dispatch: any }, { req }: { req: any }) {
+//     const user: any = getUserFromCookie(req) // JWTをデコードしたトークンを取得
+//     if (user) {
+//       await dispatch('user/setUser', {
+//         name: user.displayName,
+//         email: user.email,
+//         avatar: user.photoURL,
+//         uid: user.user_id,
+//       })
+//     }
+//   },
+// }
